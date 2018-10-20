6bd4194bb6b0b5b10aaa294513d5b9cae03c534c
ahmad019 <ahmdbilx@gmail.com>
2018-10-20 16:20:58 +0500

    x and y labels

--- transmission.py
+++ transmission.py
@@ -9,26 +9,32 @@ import matplotlib.pyplot as plt
 
 t = 21
 t2 = 12
-ra = 41
+ran = 410
 phi = -1
 r = 11
 r2 = 15
-Ei = np.ndarray(ra, float)
-E = np.ndarray(ra, float)
-phit = np.ndarray(ra, float)
+Etai = np.ndarray(ran, float)
+Etsi = np.ndarray(ran, float)
+phit = np.ndarray(ran, float)
 
-for i in range(0,ra):
+for i in range(0,ran):
     
-    Et = ((1-t*t2)*np.exp(1j*phi/2))/(1-(r*r2)*np.exp(1j*phi))
-    Et1 = Et.conjugate()
+    Eta = ((1-t*t2)*np.exp(1j*phi/2))/(1-(r*r2)*np.exp(1j*phi)) #asymmeteric
+    Ets = ((1-t*t)*np.exp(1j*phi/2))/(1-(r*r)*np.exp(1j*phi)) #symmeteric
+    EtaC = Eta.conjugate()
+    EtsC = Ets.conjugate()
     
     phi = phi + 0.05
-    Ei[i] = abs(Et*Et1)
-    #E[i] = Et.real
+    Etai[i] = abs(Eta*EtaC)
+    Etsi[i] = abs(Ets*EtsC)
     phit[i] = phi
 
-plt.xlim([-2,2])
-plt.plot(phit,Ei, 'm')
-#plt.plot(phit,Ei, 'm')
+#plt.xlim([-2,2])
+#plt.legend('r=r')
+plt.title('Transmission Curve of Fabry-Perot Resonator')
+plt.xlabel('Round trip/Resonant Phase')
+plt.ylabel('Transmitted Energy (mod squared)')
+plt.plot(phit,5.70*Etai, 'm') #asymmeteric
+plt.plot(phit,Etsi, 'r') #symmeteric
 
 plt.show()
