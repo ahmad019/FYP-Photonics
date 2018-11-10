ddd63feb42898badb893aee21a32b02a85647383
ahmad019 <ahmdbilx@gmail.com>
2018-11-10 16:06:50 +0500

    Distinct plots for Transmission and Reflection

--- ImagvsReal.py
+++ ImagvsReal.py
@@ -9,8 +9,8 @@ Created on Tue Nov  6 14:43:18 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-ran = 200
-phi = -1
+ran = 90000
+phi = 0
 r = 0.98797
 r2 = 0.97889
 
@@ -18,9 +18,10 @@ t = np.sqrt(1-(r**2))
 t2 = np.sqrt(1-(r2**2))
 
 
-Etai = np.ndarray(ran, float)
-Erai = np.ndarray(ran, float)
-Ersi = np.ndarray(ran, float)
+Etii = np.ndarray(ran, float)
+Etri = np.ndarray(ran, float)
+Erri = np.ndarray(ran, float)
+Erii = np.ndarray(ran, float)
 phit = np.ndarray(ran, float)
 
 for i in range(0,ran):
@@ -30,20 +31,33 @@ for i in range(0,ran):
     Eta = -(t*t2*np.exp(1j*phi/2))/(1-r*r2*np.exp(1j*phi)) #asymmeteric transmission
 
     
-    phi = phi + 0.01    
+    phi = phi + 0.001
     
-    Etai[i] = Eta.real #abs(Eta*EtaC)
-    Erai[i] = Eta.imag #abs(Era*EraC)
-    Ersi[i] = Etai[i] + Erai[i]
+    Etri[i] = Eta.real
+    Etii[i] = Eta.imag
+    Erri[i] = Era.real
+    Erii[i] = Era.imag
     phit[i] = phi
+    
+    
 
-#plt.xlim([-1,1])
-#plt.ylim([-2,2])
+plt.xlim([-1,1])
+plt.ylim([-0.7,0.7])
 
 
-plt.title('Imaginary vs Real')
+plt.title('Transmision Imaginary vs Real')
 plt.xlabel('Real axis')
 plt.ylabel('Imaginary axis')
 
-plt.plot(Etai,Erai, 'b')
+plt.plot(Etri,Etii, 'c')
+plt.show()
+
+plt.xlim([-0.5,1.2])
+plt.ylim([-0.5,0.5])
+
+plt.title('Reflection Imaginary vs Real')
+plt.xlabel('Real axis')
+plt.ylabel('Imaginary axis')
+
+plt.plot(Erri,Erii, 'b')
 plt.show()
