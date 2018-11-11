0e47b67aebf049d9006e3b479c5bcbc71cd07f5d
ahmad019 <ahmdbilx@gmail.com>
2018-11-11 21:17:57 +0500

    added limits for 0 to 2pi

--- ImagvsReal.py
+++ ImagvsReal.py
@@ -9,7 +9,7 @@ Created on Tue Nov  6 14:43:18 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-ran = 90000
+ran = 62840 #for 2pi limit set to 6284
 phi = 0
 r = 0.98797
 r2 = 0.97889
@@ -45,9 +45,10 @@ plt.xlim([-1,1])
 plt.ylim([-0.7,0.7])
 
 
-plt.title('Transmision Imaginary vs Real')
+plt.title('Transmision Imaginary vs Real (for limit 0 to 2pi)')
 plt.xlabel('Real axis')
 plt.ylabel('Imaginary axis')
+plt.grid()
 
 plt.plot(Etri,Etii, 'c')
 plt.show()
@@ -55,9 +56,10 @@ plt.show()
 plt.xlim([-0.5,1.2])
 plt.ylim([-0.5,0.5])
 
-plt.title('Reflection Imaginary vs Real')
+plt.title('Reflection Imaginary vs Real (for limit 0 to 2pi)')
 plt.xlabel('Real axis')
 plt.ylabel('Imaginary axis')
+plt.grid()
 
 plt.plot(Erri,Erii, 'b')
 plt.show()
