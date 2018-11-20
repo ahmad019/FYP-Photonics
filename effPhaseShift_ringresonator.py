3ba1274a04dc31d269d627ac5d2ec37bd1f97253
ahmad019 <ahmdbilx@gmail.com>
2018-11-20 12:59:24 +0500

    added code for effective phase of ring resonator

--- effPhaseShift_ringresonator.py
+++ effPhaseShift_ringresonator.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 ran = 6284
 phi = -np.pi
-r = 0.9989
+r  = 0.9989
 r2 = 0.7541
 r3 = 0.5012
 r4 = 0.2592
@@ -29,7 +29,7 @@ phit = np.ndarray(ran, float)
 
 for i in range(0,ran):
     
-    PHI = np.pi + phi + 2*np.arctan((r*np.sin(phi))/(1-r*np.cos(phi)))
+    PHI  = np.pi + phi + 2*np.arctan((r*np.sin(phi))/(1-r*np.cos(phi)))
     PHI2 = np.pi + phi + 2*np.arctan((r2*np.sin(phi))/(1-r2*np.cos(phi)))
     PHI3 = np.pi + phi + 2*np.arctan((r3*np.sin(phi))/(1-r3*np.cos(phi)))
     PHI4 = np.pi + phi + 2*np.arctan((r4*np.sin(phi))/(1-r4*np.cos(phi)))
@@ -49,9 +49,9 @@ for i in range(0,ran):
 #plt.ylim([0.6,1.05])
 
 
-plt.title('all pass')
-plt.xlabel('single-pass phase')
-plt.ylabel('effective phase shift')
+plt.title('All-pass ring resonator')
+plt.xlabel('Single-pass phase')
+plt.ylabel('Effective phase shift')
 plt.plot(phit,PHIi, 'm')
 plt.plot(phit,PHI2i, 'orange')
 plt.plot(phit,PHI3i, 'purple')
