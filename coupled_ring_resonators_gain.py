527bf41ed5793871c8f4559b035c86f42add0fb9
ahmad <ahmdbilx@gmail.com>
2019-02-23 16:24:57 +0500

    update

--- coupled_ring_resonators_gain.py
+++ coupled_ring_resonators_gain.py
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 ran = 2000
 phi1 = -1
 phi2 = -1
-r1 =  0.943565476797
-r2 = 0.998189867767
+r1 =  0.96389793565476797
+r2 = 0.989189867767
 lamd = 0.000001550
 Q = 6000000
 b = 0.000025
@@ -21,11 +21,11 @@ l = 2*np.pi*b
 n = 3.45
 
 aa = (2*np.pi*n)/(Q*lamd)
-g1 = 0.9001
+g1 = 1.9001
 aa2 = (2*np.pi*n)/(Q*lamd)
-g2 = 0.91001
+g2 = 2.91001
 aa3 = (2*np.pi*n)/(Q*lamd)
-g3 = 0.9101
+g3 = 3.9101
 a1 = np.exp(((g1-aa)*l)/2)
 a2 = np.exp(((g2-aa2)*l)/2)
 a3 = np.exp(((g3-aa3)*l)/2)
@@ -48,25 +48,22 @@ with open('Coupled_resonator_phi.phase.trans.csv', 'w') as fp:
         
         Eta = (r1-Er12*a1*np.exp(1j*phi1))/(1-r1*Er12*a1*np.exp(1j*phi1)) #transmission
         
-        #Era = (r1-r2*a1*np.exp(1j*phi1))/(1-r1*r2*a1*np.exp(1j*phi1))
+        #phi12 = np.pi + phi1 + 2*np.arctan((r1*np.sin(phi1))/(1-r1*np.cos(phi1))) #phase of r12
         
+        phi12 = np.arctan(a1*np.sin(phi1)/(r1-a3*np.cos(phi1))) + np.arctan(r1*a3*np.sin(phi1)/(1-r1*a3*np.cos(phi1)))
         
         phi1 = phi1 + 0.001    
         phi2 = phi2 + 0.001    
         
         Etai[i] = abs(Eta)**2 
-        #Erai[i] = abs(Era)**2
-        #PHI[i] = np.arctan(Eta.imag/Eta.real)
         
-        PHI[i] = np.arctan(r1*a1*abs(Er12)*np.sin(phi2+phi1))/(1-r1*a1*abs(Er12)*np.cos(phi2+phi1)) - np.arctan((a1*abs(Er12)*np.sin(phi2+phi1))/(r1-a1*abs(Er12)*np.cos(phi2+phi1)))
+        PHI[i] = np.arctan(r1*a1*abs(Er12)*np.sin(phi12+phi1))/(1-r1*a1*abs(Er12)*np.cos(phi12+phi1)) - np.arctan((a1*abs(Er12)*np.sin(phi12+phi1))/(r1-a1*abs(Er12)*np.cos(phi12+phi1)))
         
         fp.write("{},{},{}\n".format(phi1,PHI[i], Etai[i]))
         
         phi1t[i] = phi1
         phi2t[i] = phi2
         
-    
-
 
 fig, axs = plt.subplots(2)
 
@@ -75,7 +72,7 @@ axs[0].plot(phi1t,Etai, 'b')
 #plt.show()
 #fig.savefig('Triple_resonator_trans.png', dpi=400)
 
-axs[1].set_xlim([-0.15,0.15])
+#axs[1].set_xlim([-0.15,0.15])
 axs[1].set_title("Phase")
 axs[1].plot(phi2t,PHI, 'r')
 #plt.show()
@@ -83,7 +80,7 @@ axs[1].plot(phi2t,PHI, 'r')
 fig.tight_layout()
 plt.show()
 
-fig.savefig('Coupled_resonator_phase.trans.png', dpi=400)
+#fig.savefig('Coupled_resonator_phase.trans.png', dpi=400)
 
 #plt.ylim(top=1.02,bottom=0.9)
 
