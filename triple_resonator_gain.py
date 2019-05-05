875712096b5931baadb73dda1f2a5c59efd8166b
ahmad <ahmdbilx@gmail.com>
2019-05-06 04:24:32 +0500

    updated

--- triple_resonator_gain.py
+++ triple_resonator_gain.py
@@ -9,39 +9,45 @@ Created on Mon Feb 11 15:00:19 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-ran = 20000
-phi1 = -1
-phi2 = -1
-phi3 = -1
-#r1 =  0.9695465476797
-#r2 = 0.898795189867767
-r3 = 0.8968896789798389
-lamd = 0.000001550
-Q = 6000000
-b = 0.000025
+ran = 1000
+phi1 = -0.5
+phi2 = -0.5
+phi3 = -0.5
+r1 = 0.8999
+r2 = 0.9889
+r3 = 0.9989
+lamd = 1550e-9
+Q1 = 1e5
+Q2 = 1e6
+Q3 = 1e7
+b = 10e-6
 l = 2*np.pi*b
 n = 3.45
 c = 299792458
 
 tau = (n*2*np.pi*b)/c
 
-aa = (2*np.pi*n)/(Q*lamd)
-g1 = 1.90001
-aa2 = (2*np.pi*n)/(Q*lamd)
-g2 = 2.981001
-aa3 = (2*np.pi*n)/(Q*lamd)
-g3 = 4.01
-a1 = np.exp(((g1-aa)*l)/2)
-a2 = np.exp(((g2-aa2)*l)/2)
-a3 = np.exp(((g3-aa3)*l)/2)
+aa = (2*np.pi*n)/(Q1*lamd)
+g1 = 0
+aa2 = (2*np.pi*n)/(Q2*lamd)
+g2 = 0
+aa3 = (2*np.pi*n)/(Q3*lamd)
+g3 = 0
+a1 = np.exp(-(aa-g1)*l/2)
+a2 = np.exp(-(aa2-g2)*l/2)
+a3 = np.exp(-(aa3-g3)*l/2)
 
-r2 = r3*a2
-r1 = r2*a1 #critical
+g=g1+g2+g3
+#r2 = r3*a2
+#r1 = r2*a1 #critical
 
 
 Etai = np.ndarray(ran, float)
 Erai = np.ndarray(ran, float)
-PHI = np.ndarray(ran, float)
+PHIt = np.ndarray(ran, float)
+EtaImag = np.ndarray(ran, float)
+EtaReal = np.ndarray(ran, float)
+vgt = np.ndarray(ran, float)
 
 phi1t = np.ndarray(ran, float)
 phi2t = np.ndarray(ran, float)
@@ -67,55 +73,68 @@ with open('Triple_resonator_phi.phase.trans.csv', 'w') as fp:
         
         #phi12 = np.angle(Er12)
         
-        phi1 = phi1 + 0.0001    
-        phi2 = phi2 + 0.0001    
-        phi3 = phi3 + 0.0001
+        phi1 = phi1 + 0.001    
+        phi2 = phi2 + 0.001    
+        phi3 = phi3 + 0.001
         
         Etai[i] = abs(Eta)**2 
         
+        EtaImag[i] = Eta.imag
+        EtaReal[i] = Eta.real
     
-        #PHI[i] = np.angle(Eta)
+        PHIt[i] = np.angle(Eta)
+        #PHIt[i] = np.arctan((a1*abs(Er12)*np.sin(phi1+phi12))/(r1-a1*abs(Er12)*np.cos(phi1+phi12))) + np.arctan((r1*a1*abs(Er12)*np.sin(phi1+phi12))/(1-r1*a1*abs(Er12)*np.cos(phi1+phi12))) #total eff phase
         
-        PHI[i] = np.arctan((a1*abs(Er12)*np.sin(phi1+phi12))/(r1-a1*abs(Er12)*np.cos(phi1+phi12))) + np.arctan((r1*a1*abs(Er12)*np.sin(phi1+phi12))/(1-r1*a1*abs(Er12)*np.cos(phi1+phi12))) #total eff phase
+        rho = phi1 + phi12
+        
+        vg = 1/(n/c)*(((r1-a1*abs(Er12)*np.cos(rho))**2/((r1-a1*abs(Er12)*np.cos(rho))**2 + (a1*abs(Er12)*np.sin(rho))**2)) + ((1-r1*a1*abs(Er12)*np.cos(rho))**2 / ((1-r1*a1*abs(Er12)*np.cos(rho))**2 + (r1*a1*abs(Er12)*np.sin(rho))**2) ))
+        
+        #vgt[i] = vg
+        
         
         #fp.write("{},{},{}\n".format(phi1,PHI[i], Etai[i]))        
         
         phi1t[i] = phi1
         phi2t[i] = phi12
         
-        rho = phi1 + phi12
         
-    
-vg = 1/(n/c)*(((r1-a1*abs(Er12)*np.cos(rho))**2/((r1-a1*abs(Er12)*np.cos(rho))**2 + (a1*abs(Er12)*np.sin(rho))**2)) + ((1-r1*a1*abs(Er12)*np.cos(rho))**2 / ((1-r1*a1*abs(Er12)*np.cos(rho))**2 + (r1*a1*abs(Er12)*np.sin(rho))**2) ))
+dydx = np.diff(PHIt)/np.diff(phi1t)
+vgt = (1/dydx)*c/n
+ngt = dydx * n
 
 
-fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(8,4))
 
-axs[0].set_title("Transmitted field // EIA")
-axs[0].set_xlabel("Round Trip phase")
-axs[0].set_ylabel("Transmittance")
-axs[0].plot(phi1t,Etai, 'b')
+
+fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(8,5))
+
+axs[0,0].set_title("Transmitted field")
+axs[0,0].set_xlabel("Round Trip phase")
+axs[0,0].set_ylabel("Transmittance")
+axs[0,0].plot(phi1t,Etai, 'b')
 #plt.show()
 #fig.savefig('Triple_resonator_trans.png', dpi=400)
 
-#axs[1].set_xlim([-0.2,0.2])
-axs[1].set_title("Phase")
-axs[1].set_xlabel("Round Trip phase")
-axs[1].set_ylabel("Total phase")
-axs[1].plot(phi1t,PHI, 'r')
+#axs[0,1].set_xlim([-0.02,0.02])
+axs[0,1].set_title("Phase")
+axs[0,1].set_xlabel("Round Trip phase")
+axs[0,1].set_ylabel("Total phase")
+axs[0,1].plot(phi1t,PHIt, 'r')
 #plt.show()
+
+#axs[1,0].set_xlim([-0.2,0.2])
+axs[1,0].set_title("Transmission Imag vs Real")
+axs[1,0].set_xlabel("Real axis")
+axs[1,0].set_ylabel("Imaginart axis")
+axs[1,0].plot(EtaReal,EtaImag, 'y')
+
+#axs[1,1].set_xlim([-0.5,0.5])
+axs[1,1].set_title("Group Velocity")
+axs[1,1].set_xlabel("detuning")
+axs[1,1].set_ylabel("Vg")
+axs[1,1].plot(phi1t,np.append([1],vgt), 'g')
+#axs[1,1].plot(phi1t,vgt, 'g')
 
 fig.tight_layout()
 plt.show()
 
-#fig.savefig('Triple_resonator_phase.trans.png', dpi=400)
-
-
-#np.savetxt("fsoo.csv", PHI, delimiter=",")
-#np.savetxt("fsoo.csv", Etai, delimiter=",")
-
-
-#with open('fsoo.csv', 'w') as fp:
-    #np.savetxt(PHI, Etai, '%f', ',')
- #   fp.write("{},{},{}\n".format(phi1t,PHI, Etai))
-    
+fig.savefig('Triple_resonator_phase.trans%d.png'%g, dpi=400)
