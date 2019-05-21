5c62364c233f6e1844f7c613dbfce03331a0b909
ahmad <ahmdbilx@gmail.com>
2019-05-22 01:41:57 +0500

    changes

--- coupled_ring_resonators(gain_controlled_tuning_imp).py
+++ coupled_ring_resonators(gain_controlled_tuning_imp).py
@@ -8,44 +8,44 @@ Created on Wed Dec 12 13:39:29 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
-from basic_units import radians
+from basic_units import radians, degrees, cos
 
-ran = 628315
-midran = int(ran/2 -1)
-phi1 = -np.pi
-phi2 = -np.pi
-r1 =  0.8998
-r2 =  0.9998
+ran = 10000
+phi1 = -0.5
+phi2 = -0.5
+r1 = 0.98898
+r2 = 0.9999998
 lamd = 0.000001550
 Q1 = 1e5
 Q2 = 1e6
-b = 10e-6
+c = 299792458
+b = 0.000025
 l = 2*np.pi*b
 n = 3.45
-c = 299792458
+
+g1 = 0
+g2 = 14
+
 tau = (n*2*np.pi*b)/c
 
 aa = (2*np.pi*n)/(Q1*lamd)
-g1 = 0
 aa2 = (2*np.pi*n)/(Q2*lamd)
-g2 = 16
-
-g =g1+g2
-
 a1 = np.exp(-(aa-g1)*l/2)
 a2 = np.exp(-(aa2-g2)*l/2)
 
+#a1 = 0.88
+#a2 = 0.9999 boyd
+
+#r2 = a2
 #r1 = r2*a1 #critical
 
+#a1 = a1.round(decimals=2)
+#a2 = a2.round(decimals=2)
+
 Etai = np.ndarray(ran, float)
-Erai = np.ndarray(ran, float)
-PHIt = np.ndarray(ran, float)
 dPHIt = np.ndarray(ran, float)
+PHIt = np.ndarray(ran, float)
 PHI12 = np.ndarray(ran, float)
-EtaImag = np.ndarray(ran, float)
-PHIImag = np.ndarray(ran, float)
-EtaReal = np.ndarray(ran, float)
-PHIReal = np.ndarray(ran, float)
 
 phi1t = np.ndarray(ran, float)
 phi2t = np.ndarray(ran, float)
@@ -59,40 +59,31 @@ with open('Coupled_resonator_phi.phase.trans.csv', 'w') as fp:
         
         Eta = (r1-Er12*a1*np.exp(1j*phi1))/(1-r1*Er12*a1*np.exp(1j*phi1)) #transmission
         
-        #phi12 = np.pi + phi1 + 2*np.arctan((r1*np.sin(phi1))/(1-r1*np.cos(phi1))) #phase of r12
+        #Era = (r1-r2*a1*np.exp(1j*phi1))/(1-r1*r2*a1*np.exp(1j*phi1))
         
-        #phi12 = np.angle(Er12)  
+        #phi12 =  np.arctan((r2*a2*np.sin(phi2))/(1-r2*a2*np.cos(phi2))) - np.arctan((a2*np.sin(phi2))/(r2-a2*np.cos(phi2)))
         phi12 = np.arctan((a2*((r2**2) - 1)*np.sin(phi2))/(((a2**2) + 1)*r2 - a2*(r2**2 + 1)*np.cos(phi2)))
-        #phi12 = -np.arctan(a2*np.sin(phi2)/(r2-a2*np.cos(phi2))) + np.arctan(r2*a2*np.sin(phi2)/(1-r2*a2*np.cos(phi2)))
         
         #PHIt[i] = -np.arctan((a1*abs(Er12)*np.sin(phi12+phi1))/(r1-a1*abs(Er12)*np.cos(phi12+phi1))) + np.arctan((r1*a1*abs(Er12)*np.sin(phi12+phi1))/(1-r1*a1*abs(Er12)*np.cos(phi12+phi1)))
         #PHIt[i] = phi1 - (np.arctan((a2*np.sin(phi2))/(r2-a2*np.cos(phi2))) - np.arctan((a2*r2*np.sin(phi2))/(1-a2*r2*np.cos(phi2))))
         PHIt[i] = np.arctan((a1*abs(Er12)*((r1**2) - 1)*np.sin(phi1+phi12))/((a1**2)*abs(Er12)**2*r1-a1*abs(Er12)*((r1**2)+1)*np.cos(phi1+phi12)+r1))
-        #PHIt[i] = np.angle(Eta)
         
         PHI12[i] = phi12
-                
         
-        phi1 = phi1 + 0.00001    
-        phi2 = phi2 + 0.00001
+        phi1 = phi1 + 0.0001    
+        phi2 = phi2 + 0.0001    
         
         Etai[i] = abs(Eta)**2 
-        Erai[i] = abs(Er12)**2  
-        
-        EtaImag[i] = Eta.imag
-        EtaReal[i] = Eta.real
-        PHIImag[i] = Er12.real
-        PHIReal[i] = Er12.imag
         
         dPHIt[i] = ((1-r1*abs(Er12)*a1*np.cos(phi12+phi1))**2/((1-r1*abs(Er12)*a1*np.cos(phi12+phi1))**2 + (r1*abs(Er12)*a1*np.sin(phi1+phi12))**2))  - ((r1-a1*abs(Er12)*np.cos(phi12+phi1))**2 / ((r1-a1*abs(Er12)*np.cos(phi12+phi1))**2  + (abs(Er12)*a1*np.sin(phi12+phi1))**2))
         
-        #fp.write("{},{},{}\n".format(phi1,PHI[i], Etai[i]))
-        
-        phi1t[i] = phi1*2*np.pi/tau
-        phi2t[i] = phi12
-        
+        fp.write("{},{},{}\n".format(phi1,PHIt[i], Etai[i]))
+    
+        phi1t[i] = phi1
+        phi2t[i] = phi2
+    
 
-over = r1 - phi2t[midran]*a1
+over = r1 - phi2t[499]*a1
 ove = r1 - r2*a1
 ovr = r2 - a2
 
@@ -104,48 +95,48 @@ dydx = np.diff(PHIt)/np.diff(phi1t)
 #dydx = np.append(dydx, dydx[99])
 
 vgt = (1/dydx)*c/n
-ngt = dPHIt * n
+ngt = dydx * n
+
 
 #phi1t = [phi1t[i]*radians for i in range(0,ran)]
+#phi2t = [phi2t[i]*radians for i in range(0,ran)]
 
 fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(8,5))
-
-axs[0,0].set_xlim([-(np.pi)*1e12,(np.pi)*1e12])
+    
 axs[0,0].set_title("Transmitted field")
-axs[0,0].set_xlabel("Frequency detuning (THz)")
+axs[0,0].set_xlim([-0.1,0.1])
+#axs[0,0].set_ylim([0,1.2])
+axs[0,0].set_xlabel("Frequency detuning")
 axs[0,0].set_ylabel("Transmittance")
-#axs[1,0].text(0.001,0.0000,"Gain1 =%f" %g1 + "\nGain2 =%f" %g2 + "\nOver coupled\nr1=%.8f" %r1 + "\nr2=%.8f"%r2,fontsize=8)
-axs[0,0].plot(phi1t,Etai, 'b', xunits=radians)
+#axs[1,0].text(0.001,0.0000,"Gain1 =%f" %g1 + "\nGain2 =%f" %g2 + "\nUnder coupled\nr1=%.8f" %r1 + "\nr2=%.8f"%r2,fontsize=8)
+axs[0,0].plot(phi1t,Etai, 'b')
 
-axs[0,1].set_xlim([-0.5e12,0.5e12])
+axs[0,1].set_xlim([-np.pi/8,np.pi/8])
 axs[0,1].set_title("Effective Phase")
-axs[0,1].set_xlabel("Frequency detuning (THz)")
+axs[0,1].set_xlabel("Frequency detuning")
 axs[0,1].set_ylabel("Effective phase")
 axs[0,1].plot(phi1t,PHIt, 'r')
+#axs[0,1].plot(phi2t,PHI12*6, 'y')
 
-axs[1,0].set_xlim([-1,1.2])
-axs[1,0].set_ylim([-1,1])
-axs[1,0].set_title("Transmission Imag vs Real")
-axs[1,0].set_xlabel("Imaginary axis")
-axs[1,0].set_ylabel("Real axis")
-axs[1,0].plot(EtaReal,EtaImag, 'y')
+axs[1,0].set_xlim([-0.001,0.001])
+#axs[1,0].set_ylim([-0.1,0.1])
+axs[1,0].set_title("Effective Phase")
+axs[1,0].set_xlabel("Frequency detuning")
+axs[1,0].set_ylabel("Effective phase")
+axs[1,0].plot(phi1t,PHIt, 'r')
 
-axs[1,1].set_xlim([-0.5e11,0.5e11])
-axs[1,1].set_title("Effective Phase")
-axs[1,1].set_xlabel("Frequency detuning (THz)")
+axs[1,1].set_xlim([-0.01,0.01])
+axs[1,1].set_ylim([-25,50000])
+axs[1,1].set_title("Group Index (n * dΦ/dφ)")
+axs[1,1].set_xlabel("Frequency detuning")
 axs[1,1].set_ylabel("Effective phase")
-#axs[1,1].set_title("Phase Derivative")
-#axs[1,1].set_xlabel("Real axis")
-#axs[1,1].set_ylabel("Imaginary axis")
-#axs[1,1].plot(PHIReal, PHIImag , 'g')
-#axs[1,1].plot(phi1t, np.append([1],dydx) , 'g')
-axs[1,1].plot(phi1t,PHIt, 'r')
-#axs[1,1].plot(phi1t, ngt , 'g')
+#axs[1,1].plot(phi1t, n*(np.append([1],dydx)) , 'g')
+axs[1,1].plot(phi1t, np.append([1],ngt) , 'g')
 
 fig.tight_layout()
 plt.show()
 
-#fig.savefig("coupled_ring_EIA %d.png" %g,dpi=400)
+fig.savefig("coupled_ring_EIA.png",dpi=400)
 
 
-#print(over,ove,ovr)
+print(over,ove,ovr)
--- reflection+transmission_FabryPerot.py
+++ reflection+transmission_FabryPerot.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 from basic_units import radians, degrees, cos
 
 ran = 6284
-phi = -np.pi
+phi = 0
 r = 0.98797
-r2 = 0.97889
+r2 = 0.7889
 
 t = np.sqrt(1-(r**2))
 t2 = np.sqrt(1-(r2**2))
@@ -32,33 +32,33 @@ for i in range(0,ran):
     
     phi = phi + 0.001    
     
-    Etai[i] = abs(Eta)**2 #abs(Eta*EtaC)
-    Erai[i] = abs(Era)**2 #abs(Era*EraC)
-    Ersi[i] = np.angle(Era)
-    phit[i] = phi
+    Etai[i] = Eta.real #abs(Eta*EtaC)
+    Erai[i] = Eta.imag #abs(Era*EraC)
+    Ersi[i] = Era.real
+    phit[i] = Era.imag
 
 #plt.xlim([-1,1])
 #plt.ylim([-0.2,1.2])
 #plt.legend(Eta,Ets, borderpad=2)
 #plt.legend([Etai, Etsi], ["line 2", "line 1"])
 
-phit = [phit[i]*radians for i in range(0,ran)]
+#phit = [phit[i]*radians for i in range(0,ran)]
 
-plt.title('Reflection + Transmission Fabry-Perot Resonator')
-plt.xlabel('Round Trip Phase "Φ"')
-plt.ylabel('Intensity')
+plt.title('Transmission')
+plt.xlabel('Real axis')
+plt.ylabel('Imaginary axis')
 
-plt.plot(phit,Etai, 'b') #transmittance
-plt.plot(phit,Erai, 'r') #reflectance
+plt.plot(Etai,Erai, 'b') #transmittance
+#plt.plot(phit,Erai, 'r') #reflectance
 
 plt.show()
 
 fig, axs = plt.subplots(1)
 
-axs.set_title('Reflection Phase')
-axs.set_xlabel('Round Trip Phase "Φ"')
-axs.set_ylabel('Effective Phase')
-axs.plot(phit,Ersi, 'r')
+axs.set_title('Reflection')
+axs.set_xlabel('Real axis')
+axs.set_ylabel('Imaginary axis')
+axs.plot(Ersi,phit, 'r')
 
 fig.tight_layout()
 plt.show()
